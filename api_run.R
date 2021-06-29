3a65780bdd9c8bf567f689c737ed68aef3e400d3
Brandon Byrne <byrneb@uwgb.edu>
2021-06-29 11:36:26 -0500

    Amended for current project needs
    *excluded database actions

--- api_run.R
+++ api_run.R
@@ -5,13 +5,13 @@ start <- Sys.time()
 outpath <- "this is the path to the folder that will contain your downloaded files" 
 my_token <- "your token"
 db_name <- "your database name"
-conn <- dbConnect(RPostgres::Postgres(), dbname=db_name)
+# conn <- dbConnect(RPostgres::Postgres(), dbname=db_name)
 ################
 
 get_my_data(my_token, outpath, conn)
 
-update_db(conn, outpath)
-dbDisconnect(conn)
+# update_db(conn, outpath)
+# dbDisconnect(conn)
 
 time_elapse <- Sys.time() - start
 print(time_elapse)
